9d5ebc1a4bcdc9ae5b1652d51a4b7631cd4afed6
mạnh Nguyễn <nguyenmanh2004devgame@gmail.com>
2025-08-27 17:39:25 +0000

    cái chính giữa nó bị lố ra ngoài

--- src/components/tour-guide.tsx
+++ src/components/tour-guide.tsx
@@ -98,8 +98,8 @@ export default function TourGuide() {
                             </CarouselItem>
                         ))}
                     </CarouselContent>
-                    <CarouselPrevious className="absolute left-[-1rem] top-1/2 -translate-y-1/2" />
-                    <CarouselNext className="absolute right-[-1rem] top-1/2 -translate-y-1/2" />
+                    <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2" />
+                    <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2" />
                 </Carousel>
                 
                 <div className="py-2 text-center text-sm text-muted-foreground">
